ee8d43c5dee727f2cc8ed0d11114fc3b7ce388dd
Vitalii Musiienko <vitaliimusiienko10@gmail.com>
2026-02-17 11:29:15 +0200

    feat: add sqlalchemy models for Product and Category

---
+++ app/models/category.py
@@ -0,0 +1,11 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.db.base import Base
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    url: Mapped[str] = mapped_column(String, unique=True, index=True)
+    products: Mapped[list["Product"]] = relationship(back_populates="category", cascade="all, delete-orphan")
