8c638077eaf6e8267e26e201b03bd4a40cbabd90
Vitalii Musiienko <vitaliimusiienko10@gmail.com>
2026-02-19 15:24:38 +0200

    fix: delete windowsproactoreventlooppolicyy in run pyy

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
-import asyncio
 import uvicorn
 
 if __name__ == "__main__":
-    asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
-
     uvicorn.run(
         "app.main:app",
         host="localhost",
