897f0909a79b7271cea82c9a2f5abde7f1cfd99f
Vitalii Musiienko <vitaliimusiienko10@gmail.com>
2026-02-19 10:36:17 +0200

    feat: add api service for category

--- app/api/routes_categories.py
+++ app/api/routes_categories.py
@@ -1,8 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db.session import get_db
+from app.services.category_service import get_all_categories
+from app.schemas.category import CategoryResponse
 
 router = APIRouter(prefix="/categories", tags=["categories"])
 
 
-@router.get("/")
-async def get_categories():
-    return {"message": "List of categories"}
+@router.get("/", response_model=list[CategoryResponse])
+async def get_categories(db: AsyncSession = Depends(get_db)):
+    categories = await get_all_categories(db)
+    return categories
